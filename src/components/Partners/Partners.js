82b4c787284d3a77bbf1a76ca4a6e5dc928974cc
0xdanya <crazycat23031@gmail.com>
2025-03-11 17:23:34 +1000

    super final changes please

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,11 +1,11 @@
 import React, { useRef, useState, useEffect } from 'react';
 import './Partners.css';
 // Импортируйте видеофайлы, если они находятся в src
-import video1 from '../../assets/videos/video1.MP4';
-import video2 from '../../assets/videos/video2.MP4';
-import video3 from '../../assets/videos/video3.MP4';
-import video4 from '../../assets/videos/video4.MP4';
-import video5 from '../../assets/videos/video5.MP4';
+import video1 from '../../assets/videos/video1.mp4';
+import video2 from '../../assets/videos/video2.mp4';
+import video3 from '../../assets/videos/video3.mp4';
+import video4 from '../../assets/videos/video4.mp4';
+import video5 from '../../assets/videos/video5.mp4';
 import video6 from '../../assets/videos/video6.mp4';
 import video7 from '../../assets/videos/video7.mp4';
 import video8 from '../../assets/videos/video8.mp4';
